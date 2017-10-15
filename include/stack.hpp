600fe4f81112e58358b8f2c2307dd75eb9fa8272
Yana Grishkova <xyanagrishkovax@gmail.com>
2017-10-15 21:02:13 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -6,16 +6,18 @@ template <typename T>
 class stack
 {
 public:
-	stack();
-	~stack();
-	stack(stack<T> const &copy);
-	stack<T>& operator = (stack<T> const &other);
-	size_t count() const;
-	void push(T const &);
-	T pop();
-	void print_array();
-	void print_last();
-	void swap();
+	stack() noexcept;
+	~stack() noexcept;
+	stack(stack<T> const &copy) noexcept;
+	stack<T>& operator = (stack<T> const &other) noexcept;
+	size_t count() const noexcept;
+	void push(T const &) noexcept;
+	T pop() noexcept;
+	T top() noexcept;
+	void print_array() noexcept;
+	void print_last() noexcept;
+	bool empty() noexcept;
+	void swap() noexcept;
 private:
 	T* array_;
 	size_t array_size_;
@@ -23,11 +25,11 @@ private:
 };
 
 template <typename T>
-stack<T>::stack() : count_(0), array_size_(0), array_(nullptr)
+stack<T>::stack() noexcept : count_(0), array_size_(0), array_(nullptr)
 {}
 
 template <typename T>
-stack<T>::stack(stack<T> const &copy)
+stack<T>::stack(stack<T> const &copy) noexcept
 {
 	array_size_ = copy.array_size_;
 	count_ = copy.count_;
@@ -40,7 +42,7 @@ stack<T>::stack(stack<T> const &copy)
 }
 
 template <typename T>
-stack<T>& stack<T>::operator=(stack<T> const &other)
+stack<T>& stack<T>::operator=(stack<T> const &other) noexcept
 {
 	if (this != &other)
 	{
@@ -57,19 +59,19 @@ stack<T>& stack<T>::operator=(stack<T> const &other)
 
 
 template <typename T>
-stack<T>::~stack()
+stack<T>::~stack() noexcept
 {
 	delete[] array_;
 }
 
 template <typename T>
-size_t stack<T>::count() const
+size_t stack<T>::count() const noexcept
 {
 	return count_;
 }
 
 template <typename T>
-void stack<T>::push(T const &value)
+void stack<T>::push(T const &value) noexcept
 {
 	if (array_size_ == 0)
 	{
@@ -85,7 +87,7 @@ void stack<T>::push(T const &value)
 }
 
 template <typename T>
-T stack<T>::pop()
+T stack<T>::pop() noexcept
 {
 	if (count_ == 0)
 		throw std::logic_error("Stack is empty! Try again!\n");
@@ -98,7 +100,15 @@ T stack<T>::pop()
 }
 
 template <typename T>
-void stack<T>::print_array()
+T stack<T>::top() noexcept 
+{
+	T temp = array_[count_];
+
+	return temp;
+}
+
+template <typename T>
+void stack<T>::print_array() noexcept
 {
 	if (count_ == 0)
 		std::cout << "Stack is empty! Try again!\n";
@@ -110,22 +120,27 @@ void stack<T>::print_array()
 }
 
 template <typename T>
-void stack<T>::print_last()
+void stack<T>::print_last() noexcept
 {
 	if (count_ == 0)
 	{
 		std::cout << "Stack is empty! Try again!\n";
 	}
-		
+
 	else
 	{
 		std::cout << array_[--count_];
 	}
 }
 
+template <typename T>
+bool Stack<T>::empty() noexcept
+{
+	return count();
+}
 
 template <typename T>
-void stack<T>::swap()
+void stack<T>::swap() noexcept
 {
 	T* temp = new T[array_size_];
 	std::copy(array_, array_ + count_, temp);
